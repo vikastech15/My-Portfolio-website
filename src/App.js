85f39bb827775f8def3694a4b8ab0ede37bac7ab
Vikas Gupta <148651645+vikasvermaavv@users.noreply.github.com>
2025-09-14 00:34:21 +0530

    updated appjsx

--- src/App.js
+++ src/App.js
@@ -31,7 +31,6 @@ function App() {
     <div>
       <Navbar />
       <HomeBanner id = "home"/>
-<<<<<<< HEAD
        <ProjectCard
      
         projectTitle="Books On Desk"
@@ -40,8 +39,6 @@ function App() {
         deployedProjectLink="https://book-on-desk.vercel.app/"
         projectImg={require('./images/Book.png')}
       />
-=======
->>>>>>> 21e35729c4436a7158660df339dd21c448715abe
       <ProjectCard
         id = "project"
         className = "odd"
@@ -50,11 +47,7 @@ function App() {
 integrating interactive elements and a polished design to highlight professional achievements to 
 showcase projects and skills"
         projectLink="https://github.com/vikastech15/Portfolio-website-using-reactjs.git"
-<<<<<<< HEAD
         deployedProjectLink="https://vikas-gupta-portfolio.vercel.app/"
-=======
-        deployedProjectLink="https://github.com/vikastech15/Portfolio-website-using-reactjs.git"
->>>>>>> 21e35729c4436a7158660df339dd21c448715abe
         projectImg={require('./images/portfolio.png')}
       />
 
@@ -62,7 +55,6 @@ showcase projects and skills"
         projectTitle="Real-Time Quiz Application"
         projectDesc="Our real-time quiz application, built with Node.js, HTML, CSS, and JavaScript, offers an interactive and engaging platform for hosting and participating in live quizzes"
         projectLink="https://github.com/vikastech15/realtime-quiz-app-mongoDb"
-<<<<<<< HEAD
         deployedProjectLink="https://realtime-quiz-app-mongo-db.vercel.app/"
         projectImg={require('./images/quiz app.png')}
       />
@@ -75,20 +67,6 @@ showcase projects and skills"
       />
 
       
-=======
-        deployedProjectLink="https://github.com/vikastech15/realtime-quiz-app-mongoDb"
-        projectImg={require('./images/quiz app.png')}
-      />
-
-      {/* <ProjectCard
-        className = "odd"
-        projectTitle="Rog-free: accessible anytime, anywhere"
-        projectDesc="An approach to target and heal specific health conditions by transforming the living room into a yoga studio"
-        projectLink="https://github.com/Deepajha14/Rog-Free"
-        deployedProjectLink="https://deepajha14.github.io/Rog-Free/"
-        projectImg={require('./images/projecTarot.png')}
-      /> */}
->>>>>>> 21e35729c4436a7158660df339dd21c448715abe
       <AboutMe id = 'about' />
       <SkillCard id = 'skills' />
       <ContactForm id = 'contact' />
